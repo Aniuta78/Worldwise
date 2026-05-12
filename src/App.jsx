1bb2391faeb9127ad1f0409e8d895a6ab52fe066
Ana Gorohovschi <anagorohovschi@gmail.com>
2026-05-12 17:36:14 -0400

    uses Navigate component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { Navigate } from "react-router-dom";
 
 const BASE_URL = "http://localhost:8000";
 function App() {
@@ -39,10 +40,7 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
