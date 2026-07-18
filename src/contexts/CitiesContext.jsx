deecb81c189d5b2af87d2058de47d400f839dce5
Ana Gorohovschi <anagorohovschi@gmail.com>
2026-07-18 12:34:30 -0400

    base_url for github pages

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,7 +6,7 @@ import {
   useCallback,
 } from "react";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://aniuta78.github.io/Worldwise/";
 
 const CitiesContext = createContext();
 
